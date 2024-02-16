b8097e1a37315937412352f8aff4469c603e8c8b
clightly2005 <157363198+clightly2005@users.noreply.github.com>
2024-02-16 14:08:35 +0000

    update
    -chnaged colour theme
    -chnaged images
    -changed content
    -chnaged box content backgroud

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -1,11 +1,11 @@
-const myCarouselElement = document.querySelector("#carousel");
+const myCarouselElement = document.querySelector("#carousel"); //gets from index.html
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2500, // Changes the image every 3 seconds (1000 = 1 Second)
-  touch: false,
+  touch: false, //if hover over image wil pause this interval
 });
 
-//JavaScript code
+//for hamburger toggle
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
